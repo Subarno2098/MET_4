85058f06143da401052cffc3433861fec954e1fb
Subarno2098 <subarno20@gmail.com>
2022-05-03 10:51:11 +0200

    Seminar 1

---
+++ Seminar_1.R
@@ -0,0 +1,6 @@
+install.packages(c("keras", "magick"))
+library(keras)
+install_keras()
+
+# What do you understand by the term Deep Learning? What do you associate with it? 
+# What, is the difference b/w statistical modelling and machine learning modelling?
